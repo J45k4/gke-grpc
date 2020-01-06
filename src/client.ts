4ed0ec4566b7fa01faf9ffae9cca81221a1b6813
jaska <pusanteemu@gmail.com>
2020-01-06 19:18:31 +0200

    removed function

--- src/client.ts
+++ src/client.ts
@@ -1,14 +1,11 @@
 import { ChattingServiceImplementation } from "./proto";
 import { credentials } from "grpc";
 
-(async function () {
-    const client = new ChattingServiceImplementation("chatting.koodinikkarit.com:443", credentials.createSsl());
+const client = new ChattingServiceImplementation("chatting.koodinikkarit.com:443", credentials.createSsl());
 
-    const chat = client.createChatConnection();
+const chat = client.createChatConnection();
 
-    chat.write({
-        channel: "testChannel",
-        mesage: "Hello"
-    });
-
-}())
+chat.write({
+    channel: "testChannel",
+    mesage: "Hello"
+});
